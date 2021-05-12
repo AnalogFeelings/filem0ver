c7f4739d8379bf80700e21fdf7b522db99ef3311
AestheticalZ <51166756+AestheticalZ@users.noreply.github.com>
2021-05-12 18:24:43 +0200

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace FileM0ver
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.regexPattern = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.directoryButton = new System.Windows.Forms.Button();
+            this.directoryBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.quitButton = new System.Windows.Forms.Button();
+            this.orderButton = new System.Windows.Forms.Button();
+            this.copyCheckbox = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.regexPattern);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.directoryButton);
+            this.groupBox1.Controls.Add(this.directoryBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(493, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Settings";
+            // 
+            // regexPattern
+            // 
+            this.regexPattern.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.regexPattern.Location = new System.Drawing.Point(9, 71);
+            this.regexPattern.Name = "regexPattern";
+            this.regexPattern.Size = new System.Drawing.Size(478, 20);
+            this.regexPattern.TabIndex = 4;
+            this.regexPattern.Text = "Parte\\d+\\.pdf$";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Regex pattern (Advanced!)";
+            // 
+            // directoryButton
+            // 
+            this.directoryButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.directoryButton.Location = new System.Drawing.Point(412, 30);
+            this.directoryButton.Name = "directoryButton";
+            this.directoryButton.Size = new System.Drawing.Size(75, 23);
+            this.directoryButton.TabIndex = 2;
+            this.directoryButton.Text = "Browse";
+            this.directoryButton.UseVisualStyleBackColor = true;
+            this.directoryButton.Click += new System.EventHandler(this.directoryButton_Click);
+            // 
+            // directoryBox
+            // 
+            this.directoryBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.directoryBox.Location = new System.Drawing.Point(9, 32);
+            this.directoryBox.Name = "directoryBox";
+            this.directoryBox.Size = new System.Drawing.Size(397, 20);
+            this.directoryBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Working directory";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.copyCheckbox);
+            this.groupBox2.Controls.Add(this.quitButton);
+            this.groupBox2.Controls.Add(this.orderButton);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 118);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(493, 51);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Actions";
+            // 
+            // quitButton
+            // 
+            this.quitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quitButton.Location = new System.Drawing.Point(90, 19);
+            this.quitButton.Name = "quitButton";
+            this.quitButton.Size = new System.Drawing.Size(75, 23);
+            this.quitButton.TabIndex = 1;
+            this.quitButton.Text = "Quit";
+            this.quitButton.UseVisualStyleBackColor = true;
+            this.quitButton.Click += new System.EventHandler(this.quitButton_Click);
+            // 
+            // orderButton
+            // 
+            this.orderButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.orderButton.Location = new System.Drawing.Point(9, 19);
+            this.orderButton.Name = "orderButton";
+            this.orderButton.Size = new System.Drawing.Size(75, 23);
+            this.orderButton.TabIndex = 0;
+            this.orderButton.Text = "Order";
+            this.orderButton.UseVisualStyleBackColor = true;
+            this.orderButton.Click += new System.EventHandler(this.orderButton_Click);
+            // 
+            // copyCheckbox
+            // 
+            this.copyCheckbox.AutoSize = true;
+            this.copyCheckbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.copyCheckbox.Location = new System.Drawing.Point(171, 23);
+            this.copyCheckbox.Name = "copyCheckbox";
+            this.copyCheckbox.Size = new System.Drawing.Size(134, 17);
+            this.copyCheckbox.TabIndex = 2;
+            this.copyCheckbox.Text = "Copy instead of move?";
+            this.copyCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 179);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "FileM0ver";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox directoryBox;
+        private System.Windows.Forms.Button directoryButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox regexPattern;
+        private System.Windows.Forms.Button quitButton;
+        private System.Windows.Forms.Button orderButton;
+        private System.Windows.Forms.CheckBox copyCheckbox;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace FileM0ver
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void directoryButton_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowser = new FolderBrowserDialog())
+            {
+                DialogResult dialogResult = folderBrowser.ShowDialog();
+
+                if (dialogResult == DialogResult.OK && !string.IsNullOrWhiteSpace(folderBrowser.SelectedPath))
+                {
+                    if (!Directory.EnumerateFiles(folderBrowser.SelectedPath).Any())
+                    {
+                        MessageBox.Show("Selected folder is empty and has no files.", "Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+
+                    directoryBox.Text = folderBrowser.SelectedPath;
+                }
+            }
+        }
+
+        private void orderButton_Click(object sender, EventArgs e)
+        {
+            DisableControls();
+            if (!Directory.Exists(directoryBox.Text))
+            {
+                MessageBox.Show("Selected folder does not exist.", "Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                EnableControls();
+                return;
+            }
+
+            Regex fileRegex;
+            try
+            {
+                fileRegex = new Regex(regexPattern.Text);
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("Regex pattern is invalid.", "Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                EnableControls();
+                return;
+            }
+
+            IEnumerable<string> matchingFiles = Directory.EnumerateFiles(directoryBox.Text).Where(file => fileRegex.IsMatch(file));
+            foreach (string singleFile in matchingFiles)
+            {
+                //Absolute clusterfuck
+                string cutFile = singleFile.Substring(singleFile.LastIndexOf("_") + 1).Trim(); //Remove everything leaving
+                                                                                               //only "ParteX.pdf"
+                cutFile = cutFile.Substring(0, cutFile.LastIndexOf(".")).Trim();               //Removes the ".pdf"
+
+                string correspondingFolder = Path.Combine(directoryBox.Text, cutFile);
+
+                Directory.CreateDirectory(correspondingFolder);
+                if (!Directory.Exists(correspondingFolder))
+                {
+                    MessageBox.Show($"Could not create folder for \"{cutFile}\". Please run the program as administrator.",
+                        "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    EnableControls();
+                    return;
+                }
+
+                try
+                {
+                    if (copyCheckbox.Checked)
+                    {
+                        File.Copy(singleFile, Path.Combine(correspondingFolder, Path.GetFileName(singleFile)));
+                    }
+                    else File.Move(singleFile, Path.Combine(correspondingFolder, Path.GetFileName(singleFile)));
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show(exception.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    EnableControls();
+                    return;
+                }
+            }
+
+            MessageBox.Show("Done!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            EnableControls();
+        }
+
+        public void DisableControls()
+        {
+            directoryBox.Enabled = false;
+            directoryButton.Enabled = false;
+            regexPattern.Enabled = false;
+            orderButton.Enabled = false;
+            quitButton.Enabled = false;
+        }
+
+        public void EnableControls()
+        {
+            directoryBox.Enabled = true;
+            directoryButton.Enabled = true;
+            regexPattern.Enabled = true;
+            orderButton.Enabled = true;
+            quitButton.Enabled = true;
+        }
+
+        private void quitButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Do you want to quit?", "Warning", MessageBoxButtons.YesNo,
+                MessageBoxIcon.Warning);
+
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
